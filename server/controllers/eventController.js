df1516d9436a3a60a0d443e46b358c2f745ea04a
vamsi-nv <vamsi.nv8@gmail.com>
2025-07-10 07:17:08 +0530

    cleanedup comments

--- server/controllers/eventController.js
+++ server/controllers/eventController.js
@@ -268,39 +268,3 @@ export const rsvpEvent = async (req, res) => {
     });
   }
 };
-
-// export const getUserClubEvents = async (req, res) => {
-//   const userId = req.user._id;
-
-//   try {
-//     const userClubs = await Club.find({ members: userId }).select("_id");
-//     const clubIds = userClubs.map((club) => club._id);
-//     // const userClubs = await Club.find({
-//     //   $or: [{ members: userId }, { admins: userId }]
-//     // }).select("_id");
-//     if (clubIds.length === 0) {
-//       return res.status(200).json({
-//         success: true,
-//         message: "No Clubs found for this user",
-//         events: [],
-//       });
-//     }
-
-//     const events = await Event.find({ club: { $in: clubIds } })
-//       .populate("club", "name")
-//       .populate("createdBy", "name email profileImageUrl")
-//       .sort({ date: -1 });
-
-//     res.status(200).json({
-//       success: true,
-//       events,
-//     });
-//   } catch (error) {
-//     console.error("Error in getUserClubEvents controller:", error.message);
-//     res.status(500).json({
-//       success: false,
-//       message: "Failed to fetch user club events.",
-//       error: error.message,
-//     });
-//   }
-// };
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -50,43 +50,6 @@ export const registerUser = async (req, res) => {
   }
 };
 
-// export const loginUser = async (req, res) => {
-//   if (!req.body.email || !req.body.password) {
-//     return res.status(400).json({
-//       success: false,
-//       message: "Email and password are required",
-//     });
-//   }
-
-//   const email = req.body.email.trim();
-//   const password = req.body.password.trim();
-
-//   try {
-//     const user = await User.findOne({ email });
-
-//     if (!user || !(await user.comparePassword(password))) {
-//       return res.status(401).json({
-//         success: false,
-//         message: "Invalid credentials",
-//       });
-//     }
-
-//     res.status(200).json({
-//       success: true,
-//       message: "User login successfull",
-//       user,
-//       token: generateToken(user._id),
-//     });
-//   } catch (error) {
-//     console.error("Error in loginUser controller : ", error.message);
-//     res.status(500).json({
-//       success: false,
-//       message: "Server Error",
-//       error: error.message,
-//     });
-//   }
-// };
-
 export const loginUser = async (req, res) => {
   const { email, password } = req.body;
 
@@ -135,31 +98,6 @@ export const loginUser = async (req, res) => {
     });
   }
 };
-
-// export const getCurrentUser = async (req, res) => {
-//   try {
-//     const user = await User.findById(req.user._id).select("-password");
-
-//     if (!user) {
-//       return res.status(404).json({
-//         success: false,
-//         message: "User not found",
-//       });
-//     }
-
-//     res.status(200).json({
-//       success: true,
-//       user,
-//     });
-//   } catch (error) {
-//     console.error("Error in getCurrentUser controller : ", error.message);
-//     res.status(500).json({
-//       success: false,
-//       message: "Server Error",
-//       error: error.message,
-//     });
-//   }
-// };
 
 export const getCurrentUser = async (req, res) => {
   if (!req.user || !req.user._id) {
