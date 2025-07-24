51590a5df4f926330c2b4c70648a7ddecb70eb4e
vamsi-nv <vamsi.nv8@gmail.com>
2025-07-24 21:12:14 +0530

    ~

--- client/src/utils/socket.js
+++ client/src/utils/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3000");
+const socket = io("https://csphere-server.onrender.com/");
 
 export const registerSocket = (userId) => {
   socket.emit("register", userId);
